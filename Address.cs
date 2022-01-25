b7666dde44ddfba698858cb07c3960fa6b021246
Scott Burton <burtons@byui.edu>
2022-01-25 10:02:23 -0700

    Changes we made in class

--- Address.cs
+++ Address.cs
@@ -4,6 +4,47 @@ namespace Cse210_Unit02_Ta
 {
     public class Address
     {
-        
+        // public List<int> _dice = new List<int>();
+
+        public string _street;
+        public string _city;
+        public string _state;
+
+        public Address()
+        {
+            _street = "Unknown";
+            _city = "[city]";
+            _state = "NA";
+        }
+
+        public void DisplayMailingLabel(bool isSeparateLine)
+        {
+            Console.WriteLine(_street);
+
+            if (isSeparateLine)
+            {
+                Console.WriteLine(_city);
+                Console.WriteLine(_state);
+            }
+            else
+            {
+                Console.WriteLine($"{_city}, {_state}");
+
+            }
+        }
+
+        public void PromptUserForData()
+        {
+            Console.WriteLine("What is your state? ");
+            string userState = Console.ReadLine();
+
+            if (userState == "Idaho")
+            {
+                _state = "ID";
+            }
+
+            Console.WriteLine("What is your street? ");
+            _street = Console.ReadLine();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,28 @@ namespace Cse210_Unit02_Ta
         public static void Main()
         {
             Console.WriteLine("Welcome to the program.");
+
+            Address home = new Address();
+
+            // Console.WriteLine("What is your street? ");
+            // home._street = Console.ReadLine();
+
+            home._street = "123 North Street";
+            home._city = "Rigby";
+            home._state = "ID";
+
+
+            home.DisplayMailingLabel(true);
+
+            Address work = new Address();
+            work._street = "STC 320Q";
+            work._city = "Rexburg";
+            work._state = "ID";
+
+            work.DisplayMailingLabel(true);
+
+            Address vacation = new Address();
+            vacation.DisplayMailingLabel(false);
         }
     }
 }
